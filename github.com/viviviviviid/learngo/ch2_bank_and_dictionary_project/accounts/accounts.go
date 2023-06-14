c148eed12cb0d42a9db57049ebf383b0e1369d8e
taptap-kor <tyy8282@gmail.com>
2023-06-15 02:11:42 +0900

    add: ch2-3 method

--- github.com/viviviviviid/learngo/ch2_bank_and_dictionary_project/accounts/accounts.go
+++ github.com/viviviviviid/learngo/ch2_bank_and_dictionary_project/accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "errors"
+
 /// @title public한 struct, 즉 아무나 접근 가능
 // Account struct
 // type Account struct {
@@ -29,11 +31,39 @@ func NewAccount(owner string) *Account {
 // func 다음에 나오는 (a Account)는 Receiver로 이름을 아무렇게나 지을수는 있지만,
 // 규칙에 의거하여 현재 이용하려는 struct인 Account의 첫글자인 A의 소문자를 사용해야함. ex (b, Banking)
 // Deposit x amount on your amount.
-func (a Account) Deposit(amount int) {
+// func (a Account) Deposit(amount int) {
+// 	a.balance += amount
+// }
+// Balance of your Account
+// func (a Account) Balance() int {
+// 	return a.balance
+// }
+
+// / @title 위 'method' 만들기에서 문제점 찾기 + pointer Receiver
+// / @dev 기존 방식으로는 deposit을 해도 이 함수 내에서만 적용된 것처럼 보임. 이걸 해결할 것임.
+// Deposit x amount on your amount.
+func (a *Account) Deposit(amount int) {
+	// Receiver에서 Account 앞에 *가 붙었는데, 이게 큰 차이점을 가짐. => "pointer receiver"
+	// 이 pointer receiver 있다면, account.Deposit(10)에서 account의 값을 직접 변경하는 것이고,
+	// 이게 없다면 이 메소드를 호출한 코드라인인 account.Deposit(10)에서 account의 복사본을 가져와 쓰는 것임.
+	// 없을때에는 복사본의 값을 증가시키기에, 실제 바뀐게 없을 것임. 즉 Balance() 메소드를 이용해도 그대로인 것.
 	a.balance += amount
 }
 
 // Balance of your Account
-func (a Account) Balance() int {
+func (a Account) Balance() int { // 얘는 반환만 하는 것이므로 굳이 직접 접근할 필요가 없음
 	return a.balance
+}
+
+// / @title 에러핸들링
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error { // 에러를 리턴
+	// golang에는 try-catch문이 없음
+	if a.balance < amount {
+		var errNoMoney = errors.New("Can't withdraw") // 외부에 둬도 상관없음 // 보기편하게 갖다 놓은 것 뿐
+		// 위와같이 에러변수의 이름은 err000 이런형태가 되어야함
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil // null, none과 같음
 }
