75277c6c0e2fd3fe9ad8bcfe157432176e1671e8
taptap-kor <tyy8282@gmail.com>
2023-06-17 23:30:17 +0900

    ch3 start

---
+++ github.com/viviviviviid/learngo/ch3_url_checker_and_go_routines/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+}
