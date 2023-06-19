ecbae8d377e20d8099c39af64a628faf6c2ea04b
taptap-kor <tyy8282@gmail.com>
2023-06-19 23:23:58 +0900

    feat: ch4-2 done

--- github.com/viviviviviid/learngo/ch4_job_scrapper/main.go
+++ github.com/viviviviviid/learngo/ch4_job_scrapper/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
+var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?=&searchword=blockchain&recruitSort=relation&recruitPageCount=50"
 
 func main() {
-	getPages()
+	totlaPages := getPages()
+
+	for i := 0; i < totlaPages; i++ {
+		getPage(i + 1)
+	}
+}
+
+func getPage(page int) {
+	pageURL := baseURL + "&recruitPage=" + strconv.Itoa(page) // strconv.Itoa() : go에서 지원하는 string으로 바꾸는 함수
+	fmt.Println(pageURL)
 }
 
 func getPages() int {
+	pages := 0
 	// goQuery 내용을 살펴보면, get을 사용할때 에러처리를 해줘야하고, 도큐먼트 만들때도 에러처리해줘야함
 	res, err := http.Get(baseURL)
 	checkErr(err)
@@ -26,9 +38,12 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	doc.Find(".page page_move track_event").Each()
+	// Each 위에 마우스 올려보면, 함수를 받는다는걸 확인가능. 복붙하기
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
 
-	return 0
+	return pages
 }
 
 func checkErr(err error) {
